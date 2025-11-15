51df2a6b589c7c2eb4bc7bbd93bcaf7babcfdc91
Yuvraj Rajbhar <yuvraj.raj.842@gmail.com>
2025-11-15 16:46:11 +0530

    Intial Commit

---
+++ src/main/java/com/yuvraj/taskmanager/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.yuvraj.taskmanager.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.permitAll()); // NEW syntax
+
+        return http.build();
+    }
+}
